d1e19eea082f2fb630d3186eec66cb2af18e5952
thkernel <a.dembele@upcase.net>
2022-08-16 19:56:21 +0000

    update

--- LabSys.rb
+++ LabSys.rb
@@ -8,7 +8,7 @@ def test
 	#For serial configuration.
 	#serial_connections = [{port_address: '/dev/ttyACM0', baud_rate: 9600, parity: 8}]
 	# Using ethernet configuration.
-	ethernet_connections = [{:server_ip => "192.168.1.175", :server_port => 3000}]
+	ethernet_connections = [{:server_ip => "192.168.1.121", :server_port => 3000}]
 	server = AstmServer.new(ethernet_connections,[])
 	server.start_server
 end
